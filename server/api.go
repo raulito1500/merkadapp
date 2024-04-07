64eb316963da90b2698a0ec5d89d030eff051315
Raúl Cuéllar <raulito1500@gmail.com>
2024-04-06 21:08:51 -0500

    Port vars

--- server/api.go
+++ server/api.go
@@ -31,7 +31,7 @@ func (api *Api) Run() {
 
 	server := gin.Default()
 	api.initHandlers(db, server)
-	server.Run(":8080")
+	server.Run(config.Port)
 }
 
 func (api *Api) initHandlers(db *mongo.Database, r *gin.Engine) {
